9c3446a714f5bd90475733cdc7a8b8b389df465e
Nitharsana99 <134854646+Nitharsana99@users.noreply.github.com>
2023-05-29 01:02:04 -0700

    My firstprogramme

---
+++ My firstprogram.py3.py
@@ -0,0 +1,12 @@
+Python 3.9.13 (tags/v3.9.13:6de2ca5, May 17 2022, 16:36:42) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> age=12
+>>> print(age)
+12
+>>> weight=49
+>>> print(weight)
+49
+>>> height=163
+>>> print(height)
+163
+>>> 
